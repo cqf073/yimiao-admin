2c8c23caf6a46f39cb263c7fe835db61918ba9bb
cqf073 <1730581444@qq.com>
2023-02-25 19:47:29 +0800

    完成了nginx和redis缓存及其展示

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -1,5 +1,14 @@
 <template>
       <div class="app-container">
+        <div class="el-toolbar">
+            <div class="el-toolbar-body" style="justify-content: flex-start;">
+                <a href="http://localhost:8202/admin/cmn/dict/exportDictExcel" target="_balnk">
+                    <el-button type="text"><i class="fa fa-plus"/> 导出</el-button>
+                </a>
+            <el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button>
+            </div>
+        </div>
+
         <el-table
         :data="list"
         style="width: 100%"
@@ -30,6 +39,28 @@
             </template>
             </el-table-column>
         </el-table>
+
+        <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+        <el-form label-position="right" label-width="170px">
+        <el-form-item label="文件">
+        <el-upload
+        :multiple="false"
+        :on-success="onUploadSuccess"
+        :action="'http://localhost:8202/admin/cmn/dict/importDict'"
+        class="upload-demo">
+        <el-button size="small" type="primary">点击上传</el-button>
+        <div slot="tip" class="el-upload__tip">只能上传excel文件，且不超过500kb</div>
+        </el-upload>
+        </el-form-item>
+
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false">
+            取消
+        </el-button>
+        </div>
+        </el-dialog>
+
     </div>
 
 </template>
@@ -39,7 +70,10 @@ import dict from '@/api/dict'
 export default {
     data() {
         return {
-           list:[] 
+           list:[] ,
+           listLoading: true,
+           dialogImportVisible :false
+           
         }
     },
 
@@ -48,6 +82,20 @@ export default {
     },
 
     methods: {
+        // 上传成功调用
+        onUploadSuccess(){
+            this.dialogImportVisible = false
+             this.getDictList(1)
+        },
+        //导入数据字典
+        importData(){
+             this.dialogImportVisible = true
+        },
+        // 导出数据字典
+        exportData(){
+            window.location.href="http://localhost:8202/admin/cmn/dict/exportDictExcel"
+            //  window.location.href="www.baidu.com"
+        },
 
         getDictList(id){    
             dict.DictList(id).then(Response =>{
